1acaf9dba4db953dc58412cf7a9b3f0bf219a1d5
Sergey Brazhnik <sbrazhnik@yandex-team.ru>
2018-10-26 13:44:06 +0300

    Python 3.4 removed

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,10 +2,6 @@ IF "%PY_VER%"=="2.7" (
 	%PYTHON% -m pip install --no-deps https://pypi.org/packages/cp27/c/catboost/catboost-%PKG_VERSION%-cp27-none-win_amd64.whl
 )
 
-IF "%PY_VER%"=="3.4" (
-	%PYTHON% -m pip install --no-deps https://pypi.org/packages/cp34/c/catboost/catboost-%PKG_VERSION%-cp34-none-win_amd64.whl
-)
-
 IF "%PY_VER%"=="3.5" (
 	%PYTHON% -m pip install --no-deps https://pypi.org/packages/cp35/c/catboost/catboost-%PKG_VERSION%-cp35-none-win_amd64.whl
 )
