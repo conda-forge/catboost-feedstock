87d347680ad3fc199e4a740a2eba9e01e5956dde
Marcelo Trevisani <marceloduartetrevisani@gmail.com>
2020-06-01 21:35:51 +0100

    Add more options to make

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,4 +2,4 @@
 
 pushd catboost/python-package/catboost
 
-$PYTHON ../../../ya make -DPYTHON_CONFIG=$PREFIX/bin/python3-config -DUSE_ARCADIA_PYTHON=no -DOS_SDK=local 
+$PYTHON ../../../ya make -DPYTHON_CONFIG=$PREFIX/bin/python3-config -DUSE_ARCADIA_PYTHON=no -DOS_SDK=local -j${CPU_COUNT} --c-compiler=$CC --cxx-compiler=$CXX
