77b6c6a096bce1dc324421b8381bda59b6fb1f82
akhropov <akhropov@yandex-team.ru>
2023-05-02 14:52:47 +0400

    Add linux aarch64 platform

--- recipe/build.sh
+++ recipe/build.sh
@@ -17,16 +17,22 @@ if [ `uname` == Darwin ]; then
 fi
 
 if [ `uname` == Linux ]; then
+    if [ "$PLATFORM" == "linux-aarch64" ]; then
+        TARGET_ARCH=aarch64
+    elif [ "$PLATFORM" == "linux-x86_64" ]; then
+        TARGET_ARCH=x86_64
+    fi
+
     if [ "$PY_VER" == "3.7" ]; then
-        WHL_FILE=https://pypi.org/packages/cp37/c/catboost/catboost-${PKG_VERSION}-cp37-cp37m-manylinux2014_x86_64.whl
+        WHL_FILE=https://pypi.org/packages/cp37/c/catboost/catboost-${PKG_VERSION}-cp37-cp37m-manylinux2014_${TARGET_ARCH}.whl
     elif [ "$PY_VER" == "3.8" ]; then
-        WHL_FILE=https://pypi.org/packages/cp38/c/catboost/catboost-${PKG_VERSION}-cp38-cp38-manylinux2014_x86_64.whl
+        WHL_FILE=https://pypi.org/packages/cp38/c/catboost/catboost-${PKG_VERSION}-cp38-cp38-manylinux2014_${TARGET_ARCH}.whl
     elif [ "$PY_VER" == "3.9" ]; then
-        WHL_FILE=https://pypi.org/packages/cp39/c/catboost/catboost-${PKG_VERSION}-cp39-cp39-manylinux2014_x86_64.whl
+        WHL_FILE=https://pypi.org/packages/cp39/c/catboost/catboost-${PKG_VERSION}-cp39-cp39-manylinux2014_${TARGET_ARCH}.whl
     elif [ "$PY_VER" == "3.10" ]; then
-        WHL_FILE=https://pypi.org/packages/cp310/c/catboost/catboost-${PKG_VERSION}-cp310-cp310-manylinux2014_x86_64.whl
+        WHL_FILE=https://pypi.org/packages/cp310/c/catboost/catboost-${PKG_VERSION}-cp310-cp310-manylinux2014_${TARGET_ARCH}.whl
     elif [ "$PY_VER" == "3.11" ]; then
-        WHL_FILE=https://pypi.org/packages/cp311/c/catboost/catboost-${PKG_VERSION}-cp311-cp311-manylinux2014_x86_64.whl
+        WHL_FILE=https://pypi.org/packages/cp311/c/catboost/catboost-${PKG_VERSION}-cp311-cp311-manylinux2014_${TARGET_ARCH}.whl
     fi
 fi
 
