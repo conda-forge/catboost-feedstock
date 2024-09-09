c41fa9d47e7e852aad5cecb582c87d31affa07fa
Isuru Fernando <isuruf@gmail.com>
2024-09-09 17:26:46 -0500

    use make

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 cmake ${CMAKE_ARGS} \
-  -G Ninja \
   -DCMAKE_POSITION_INDEPENDENT_CODE=On \
   -DCMAKE_BUILD_TYPE=Release \
   -DCATBOOST_COMPONENTS="PYTHON-PACKAGE" \
   .
 
-ninja -j${CPU_COUNT} _catboost _hnsw
+make -j${CPU_COUNT} _catboost _hnsw
 
 cd catboost/python-package/
 $PYTHON mk_wheel.py --build-with-cuda=no
