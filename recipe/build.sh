c3abe21e590fea4ddc5a9c39766b557fcc64b7ab
Isuru Fernando <isuruf@gmail.com>
2024-09-26 20:33:23 -0500

    cuda

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,18 @@
 mkdir bin
 ln -sf ${BUILD_PREFIX}/bin/{swig,ragel,yasm} bin/
 
+if [[ "$target_platform" == "linux-"* ]]; then
+  ln -sf $BUILD_PREFIX/bin/clang $BUILD_PREFIX/bin/${BUILD}-clang++
+  ln -sf $BUILD_PREFIX/bin/clang $BUILD_PREFIX/bin/${BUILD}-clang
+  ln -sf $BUILD_PREFIX/bin/clang $BUILD_PREFIX/bin/${HOST}-clang++
+  ln -sf $BUILD_PREFIX/bin/clang $BUILD_PREFIX/bin/${HOST}-clang
+  export CC=${HOST}-clang
+  export CXX=${HOST}-clang++
+  export CC_FOR_BUILD=${BUILD}-clang
+  export CXX_FOR_BUILD=${BUILD}-clang++
+  export NVCC_PREPEND_FLAGS="-ccbin=$BUILD_PREFIX/bin/${HOST}-clang++"
+fi
+
 if [[ "$target_platform" != "$build_platform" ]]; then
   (
     mkdir native-build
@@ -36,6 +48,15 @@ if [[ "$target_platform" != "$build_platform" ]]; then
   )
   export CMAKE_ARGS="${CMAKE_ARGS} -DTOOLS_ROOT=$SRC_DIR/native-build"
 fi
+
+if [[ "$cuda_compiler_version" != "11.8" ]]; then
+  find . -name "CMakeLists*.txt" -type f -print0 | xargs -0 sed -i "s/sm_35/sm_50/g"
+  find . -name "CMakeLists*.txt" -type f -print0 | xargs -0 sed -i "s/compute_35/compute_50/g"
+fi
+
+find . -name "CMakeLists*.txt" -type f -print0 | xargs -0 sed -i "s/-lcudart_static/-lcudart/g"
+find . -name "CMakeLists*.txt" -type f -print0 | xargs -0 sed -i "s/-lcudadevrt/-lcudart/g"
+find . -name "CMakeLists*.txt" -type f -print0 | xargs -0 sed -i "s/-lculibos/-lcudart/g"
 
 cmake ${CMAKE_ARGS} \
   -DCMAKE_POSITION_INDEPENDENT_CODE=On \
